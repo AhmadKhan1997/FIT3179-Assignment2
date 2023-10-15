13c030364fe0abc200f082424392789b750484eb
Muhammad Ahmad Khan <m.ahmad9117@gmail.com>
2023-10-15 21:16:35 +0800

    Update

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,13 +1,13 @@
 var vg_1 = "map.json";
 var vg_2 = "grouped-bar-chart.json";
 var vg_3 = "line-chart.json";
-vegaEmbed("#map-left", vg_1).then(function (result) {
+vegaEmbed("#map-left", vg_1,{renderer: "svg", actions: false}).then(function (result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed("#barchart", vg_2).then(function (result) {
+vegaEmbed("#barchart", vg_2,{renderer: "svg", actions: false}).then(function (result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#linechart", vg_3).then(function (result) {
+vegaEmbed("#linechart", vg_3,{renderer: "svg", actions: false}).then(function (result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
